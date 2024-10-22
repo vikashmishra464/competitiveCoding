e009cca13fd65188ca07edae60a4d4f2c0ea33ba
vikashmishra464 <vikashmishra0200@gmail.com>
2024-10-22 23:33:40 +0530

    POTD

---
+++ leetCode/SplitAStringIntotheMaxNumberOfUniqueSubstrings.cpp
@@ -0,0 +1,60 @@
+// 1593. Split a String Into the Max Number of Unique Substrings
+// Solved
+// Medium
+// Topics
+// Companies
+// Hint
+// Given a string s, return the maximum number of unique substrings that the given string can be split into.
+
+// You can split string s into any list of non-empty substrings, where the concatenation of the substrings forms the original string. However, you must split the substrings such that all of them are unique.
+
+// A substring is a contiguous sequence of characters within a string.
+
+ 
+
+// Example 1:
+
+// Input: s = "ababccc"
+// Output: 5
+// Explanation: One way to split maximally is ['a', 'b', 'ab', 'c', 'cc']. Splitting like ['a', 'b', 'a', 'b', 'c', 'cc'] is not valid as you have 'a' and 'b' multiple times.
+// Example 2:
+
+// Input: s = "aba"
+// Output: 2
+// Explanation: One way to split maximally is ['a', 'ba'].
+// Example 3:
+
+// Input: s = "aa"
+// Output: 1
+// Explanation: It is impossible to split the string any further.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 16
+
+// s contains only lower case English letters.
+
+// Solution:
+
+class Solution {
+public:
+    void solve(string &s,int idx,unordered_set<string>&st,int &mx,int cur){
+        for(int j=idx;j<s.size();j++){
+            string f=s.substr(idx,j-idx+1);
+            if(st.find(f)==st.end()){
+                st.insert(f);
+                solve(s,j+1,st,mx,cur+1);
+                st.erase(f);
+            }
+        }
+        mx=max(cur,mx);
+    }
+    int maxUniqueSplit(string s) {
+        unordered_set<string>st;
+        int mx=0;
+        solve(s,0,st,mx,0);
+        return mx;
+    
+    }
+};
